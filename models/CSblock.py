62614bc5fd40f3fd7a19a9845e0711cd7b6e4e82
Di Jiang <51045048+anniewwy@users.noreply.github.com>
2021-10-10 20:00:38 +0800

    add CSblock

---
+++ models/CSblock.py
@@ -0,0 +1,83 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.nn.init as init
+import numpy as np
+
+
+class CSblock(nn.Module):
+    def __init__(self, input_channel, output_channel, num_filter=64, nf_part=64):  # nf_part是什么
+        # ngf number of filters in the generator
+        # input_channel contains the number of input channel of (I,B',S,M)
+
+        self.in_channel = input_channel
+        self.out_channel = output_channel
+        self.nf = num_filter
+        self.nfp = nf_part
+
+        # encoder, with output (256, W/4, H/4)
+        self.img_encoder = self.encoder_model(0)
+        self.bg_encoder = self.encoder_model(1)
+        self.seg_encoder = self.encoder_model(2)
+        self.mt_encoder = self.encoder_model(3)
+
+        # combination, with output (64, W/4, H/4)
+        self.comb_back = self.combine_features()
+        self.comb_seg = self.combine_features()
+        self.comb_mt = self.combine_features()
+
+    def forward(self, image, background, segmentation, motion):
+        """
+        param:
+            the input image, background, segmentation and motion(videos only) should have same size
+
+        return:
+            the return out_feature is a torch with torch size ()
+
+        """
+        img_feature = self.img_encoder(image)         # (256, W/4, H/4)
+        bg_feature = self.bg_encoder(background)      # (256, W/4, H/4)
+        seg_feature = self.seg_encoder(segmentation)  # (256, W/4, H/4)
+        if motion:
+            mt_feature = self.mt_encoder(motion)      # (256, W/4, H/4)
+
+        ibg_feature = self.comb_back(torch.cat([img_feature, bg_feature], dim=1))    # (64, W/4, H/4)
+        ise_feature = self.comb_seg(torch.cat([img_feature, seg_feature], dim=1))    # (64, W/4, H/4)
+        if motion:
+            imt_feature = self.comb_mt(torch.cat([img_feature, mt_feature], dim=1))  # (64, W/4, H/4)
+            out_feature = torch.cat([ibg_feature, ise_feature, imt_feature], dim=1)  #
+        else:
+            out_feature = torch.cat([ibg_feature, ise_feature], dim=1)
+
+        return out_feature
+
+    def encoder_model(self, id_input):
+        encoder = nn.Sequential(
+            nn.Conv2d(self.in_channel[id_input], self.nf, kernel_size=7, padding=3, bias=True),  # 源码padding用的reflect
+            nn.BatchNorm2d(self.nf),
+            nn.ReLU(),
+            nn.Conv2d(self.nf, self.nf * 2, kernel_size=3, stride=2, padding=1, bias=True),
+            nn.BatchNorm2d(self.nf*2),
+            nn.ReLU(),
+            nn.Conv2d(self.nf * 2, self.nf * 4, kernel_size=3, stride=2, padding=1, bias=True),
+            nn.BatchNorm2d(self.nf * 4),
+            nn.ReLU()
+        )
+        return encoder
+
+    def combine_features(self):
+        combine_model = nn.Sequential(
+            nn.Conv2d(2*self.nf*4, self.nfp, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(self.nfp),
+            nn.ReLU()
+        )
+        return combine_model
+
+
+
+
+
+
+
+
+
